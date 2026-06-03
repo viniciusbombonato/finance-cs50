18a3447b6abc2b21512bfd6115ada40278f3be11
Vinícius Bombonato <viniciusm.bombonato@gmail.com>
2026-06-03 10:39:49 +0000

    api development

---
+++ apireader.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import yfinance as yf
+
+tickers = ["BTC-USD", "ETH-USD", "BNB-USD", "RP-USD", "SOL-USD"]
+data = yf.download(tickers)
+
+# Reshape data: get Close prices for each ticker and stack into a long format
+prices = data['Close'].reset_index()
+tb = prices.melt(id_vars='Date', var_name='Ticker', value_name='Price')
+print(tb)
--- project.py
+++ project.py
@@ -1,27 +1,54 @@
+from altair as alt
 import requests
 import streamlit as st
+import yfinance as yf
 
-def main():
-    payload={
-        'start': '1',
-        'limit': '10',
-        'convert': 'USD',
-        'fields': 'symbol, price, market_cap'
-    }
-    headers = {
-        'Accept': 'application/json',
-        'X-CMC_PRO_API_KEY': 'f5148270e8f24a0e95bd33d8cecee932',
-    }
-    url = 'https://pro-api.coinmarketcap.com/v3/cryptocurrency/listings/latest'
 
-    response = requests.get(url, params=payload, headers=headers)
-    content = response.json()
+
+
+def mouseover():
+    return hover = alt.selection_single(
+    fields=["date"],
+    nearest=True,
+    on="mouseover",
+    empty="none",
+)
+
+def line_chart():
+    lines = (
+        alt.Chart(stock_data, title="Evolution of stock prices")
+        .mark_line()
+        .encode(
+            x="date",
+            y="price",
+            color="symbol",
+        )
+    )
+
+def crypto_infos():
+    data = yf.Tickerts("BTC-USD", "ETH-USD", "ADA-USD").history(period="12mo")
+    cryptocurrencies = []
+
+    try:
+        response = requests.get(url, params=payload, headers=headers)
+        content = response.json()
+    except requests.exceptions.RequestException as e:
+        raise requests.HTTPError(f"An error occurred: {e}")
+    
+    data = content['data'].get('quote', [])
+    for key, value in data.items():
+        
+
 
     for item in content['data']:
-        quote = item.get('quote', [])
-        price = quote[0].get('price') if quote else None
-        price = round(price, 2)
-        print(f"{item['name']}: {price}")
+        yield quote = item.get('quote', [])
+        yield price = quote[0].get('price') if quote else None
+        yield price = round(price, 2) if price is not None
+        yield print(f"{item['name']}: {price}")   
+
+
+def main():
+    
 
 if __name__ == "__main__":
     main()
