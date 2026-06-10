dd2b3c2b52af7bb920efc344119932822181f94d
Vinícius Bombonato <viniciusm.bombonato@gmail.com>
2026-06-10 22:35:08 +0000

    moving foard to cleaning crypto data

--- apireader.py
+++ apireader.py
@@ -1,10 +1,34 @@
+import streamlit as st
 import pandas as pd
 import yfinance as yf
 
-tickers = ["BTC-USD"]
-data = yf.download(tickers)
+tickers = ["BTC-USD", "ETH-USD", "BNB-USD", "SOL-USD"]
+pd.set_option('display.max_rows', None) 
 
-# Reshape data: get Close prices for each ticker and stack into a long format
-# prices = data['Close'].reset_index()
-# tb = prices.melt(id_vars='Date', var_name='Ticker', value_name='Price')
-print(data)
+
+class CryptoData:
+    def __init__(self, tickers):
+        self.tickers = tickers
+        self.data = None
+
+    def fetch_data(self):
+        try:
+            self.data = yf.download(self.tickers, group_by="ticker", period="5d")
+        except Exception as e:
+            st.error(f"An error occurred while fetching cryptocurrency data: {e}", icon="🚨")
+            self.data = None
+
+    def process_data(self):
+        if self.data is not None:
+            try:
+                self.data = self.data.melt(var_name="Symbol", value_name="Price", ignore_index=False).reset_index()
+            except Exception as e:
+                st.error(f"An error occurred while processing cryptocurrency data: {e}", icon="🚨")
+
+
+crypto_data = CryptoData(tickers=tickers)
+crypto_data.fetch_data()
+crypto_data.process_data()
+                
+print(crypto_data.data)
+
---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+import pandas as pd
+from vega_datasets import data
+
+
+def get_data():
+    source = data.stocks()
+    source = source[source.date.gt("2004-01-01")]
+    return source
+
+stock_data = get_data().reset_index()
+
+print(stock_data)
